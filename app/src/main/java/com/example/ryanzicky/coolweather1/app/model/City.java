ddd17e407cc6b47dd602da792949f50e50d6baba
ryanzicky <514213431@qq.com>
2015-12-12 15:12:51 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ app/src/main/java/com/example/ryanzicky/coolweather1/app/model/City.java
@@ -0,0 +1,43 @@
+package com.example.ryanzicky.coolweather1.app.model;
+
+/**
+ * Created by ryanzicky on 2015/12/12.
+ */
+public class City {
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/example/ryanzicky/coolweather1/app/model/Province.java
@@ -0,0 +1,34 @@
+package com.example.ryanzicky.coolweather1.app.model;
+
+/**
+ * Created by ryanzicky on 2015/12/12.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
